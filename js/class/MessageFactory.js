6e679eff6a0f893b72db22800bc93e1460732ff1
AxelRaboit <axel@edgco.fr>
2022-10-29 13:22:51 +0200

    fix

--- js/class/MessageFactory.js
+++ js/class/MessageFactory.js
@@ -1,20 +1,49 @@
 import DOM from "../modules/dom.js";
 import Message from "./Message.js";
 
+/* function Builder(item, target) {
+    DOM.append(item.view(), target);
+}
 
-export default class MessageFactory {    
+export default class MessageFactory {   
+    static builder = (item, target) => {
+        Builder(item, target);
+    }
+
     static createError = (message, target) => {
         const item = new Message(message, '#FFFF', '#ED6454');
-        DOM.append(item.view(), target);
+        Builder(item, target);
     }
 
     static createSuccess = (message, target) => {
         const item = new Message(message, '#FFFF', '#68D9A4');
-        DOM.append(item.view(), target);
+        Builder(item, target);
     }
 
     static createWarning = (message, target) => {
         const item = new Message(message, '#FFFF', '#edc213');
+        Builder(item, target);
+    }
+} */
+
+export default class MessageFactory {   
+    
+    static builder = (item, target) => {
         DOM.append(item.view(), target);
+    }
+
+    static createError = (message, target) => {
+        const item = new Message(message, '#FFFF', '#ED6454');
+        this.builder(item, target);
+    }
+
+    static createSuccess = (message, target) => {
+        const item = new Message(message, '#FFFF', '#68D9A4');
+        this.builder(item, target);
+    }
+
+    static createWarning = (message, target) => {
+        const item = new Message(message, '#FFFF', '#edc213');
+        this.builder(item, target);
     }
 }
