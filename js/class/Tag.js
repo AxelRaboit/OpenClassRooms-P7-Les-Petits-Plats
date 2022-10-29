aeb639007837a0f0d2e23a81c07374218774bcd4
AxelRaboit <axel@edgco.fr>
2022-10-29 21:41:34 +0200

    [ADD/FIX] Possibility to remove a tag from the tag active list

--- js/class/Tag.js
+++ js/class/Tag.js
@@ -1,47 +1,26 @@
 import DOM from '../modules/DOM.js';
 import Recipe from './Recipe.js';
 
-export default class Tag {
+export default class Tag{
     constructor(type, name) {
         this.type = type;
         this.name = name.toLowerCase();
-
+    
         Tag.instances = [...Tag.instances, this];
     }
 
     static instances = [];
-    static active = [];
+    static active = []
 
     /**
-     * Element list tags element
-     * @returns {HTMLElement} Element List
-     */
-    listElement = () => {
-        // if this.listElement is already defined, we return it instead of create a new one
-        if (this.listElementResult) {
-            return this.listElementResult;
-        }
-
-        const element = document.createElement('li');
-        element.setAttribute('data-value', this.name);
-        element.innerText = this.name;
-
-        element.addEventListener('click', this.add);
-
-        this.listElementResult = element;
-
-        return element;
-    }
-
-    /**
-     * Tag element
+     * Create the view of a tag who'll be displayed in the active filters section
      * @returns {HTMLElement}
      */
     tag = () => {
 
-        // if this.tag is already defined, we return it instead of create a new one
+        // If the tag is already active, we don't create it
         if (this.tagResult) {
-            return this.tagReults;
+            return this.tagResult;
         }
 
         let element = document.createElement('li');
@@ -55,29 +34,46 @@ export default class Tag {
         element.appendChild(deleteBtn);
 
         this.tagResult = element;
-        
         return element;
     }
 
     /**
-     * Add the tag to the active tags and display it on the active tags list
+     * Create the view of a tag in the dropdown list
+     * @returns {HTMLElement}
+     */
+    listElement = () => {
+
+        // If the tag inside the list is already created, we return it
+        if (this.listElementResult) {
+            return this.listElementResult;
+        }
+
+        let element = document.createElement('li');
+        element.setAttribute('data-value', this.name);
+        element.innerText = this.name;
+
+        element.addEventListener('click', this.add);
+
+        this.listElementResult = element;
+        return element;
+    }
+
+    /**
+     * Add the tag to the active tags and display it
      */
     add = () => {
         Tag.active = [...Tag.active, this];
-
         DOM.append(this.tag(), document.getElementById('tags-list'));
         this.listElementResult.classList.add('already-selected');
     }
 
     /**
-     * Delete the tag from the active tags and remove it from the tags list
+     * Delete the tag from the active tags and remove it
      */
     delete = () => {
         let newActiveTags = Tag.active.filter(tag => tag !== this);
         Tag.active = newActiveTags;
-
         DOM.remove(this.tag());
-        search(Tag.active, Recipe.instances);
-        this.listElementRes.classList.remove('already-selected');
+        this.listElementResult.classList.remove('already-selected');
     }
 }
