1ba505920669b1bf73ba857004bec5f52f05d503
AxelRaboit <axel@edgco.fr>
2022-11-07 13:15:15 +0100

    [UPDATE] Display error mssage in search

--- js/modules/search.js
+++ js/modules/search.js
@@ -47,8 +47,9 @@ import FilterDropdown from '../class/FilterDropdown.js';
         // Update the dropdowns
         FilterDropdown.updateDropDowns();
     
-        // If no recipe match, display a message to the user that no recipe match with his search
-        if (document.querySelectorAll('.recipes-container .recipe:not(.hidden)').length === 0) {
+        // If no recipe match, display a message to the user that no recipe match with the search
+        const visibleRecipes = document.querySelectorAll('.recipe:not(.hidden)');
+        if(visibleRecipes.length === 0){
             document.querySelector('.recipes-container .empty-msg').classList.add('visible');
         } else {
             document.querySelector('.recipes-container .empty-msg').classList.remove('visible');
