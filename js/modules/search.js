cf68e1327bce3dc86aad530e125c69b205059151
AxelRaboit <axel@edgco.fr>
2022-11-19 10:44:09 +0100

    [UPDATE] search: add comments

--- js/modules/search.js
+++ js/modules/search.js
@@ -8,7 +8,9 @@ const search = (activeTags, recipes) => {
     recipes.forEach(recipe => {
         let visible = true;
 
+        // If tags are used, check if the recipe match with the tags
         visible = searchTags(activeTags, recipe, visible);
+        // If the main search bar is used, check if the recipe match with the search terms
         visible = searchMainInput(recipe, visible);
 
         recipeDisplayer(recipe, visible);
